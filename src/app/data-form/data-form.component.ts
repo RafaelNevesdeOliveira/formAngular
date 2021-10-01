ead05966857056496ce5524fd7f32bba49c0aefc
Rafael Neves <rafaelneves@MacBook-Air-de-Rafael.local>
2021-10-01 12:14:29 -0300

    Add formValidator

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-data-form',
   templateUrl: './data-form.component.html',
-  styleUrls: ['./data-form.component.css']
+  styleUrls: ['./data-form.component.css'],
 })
 export class DataFormComponent implements OnInit {
-
   formulario!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
 
   ngOnInit(): void {
     // this.formulario = new FormGroup({
@@ -19,9 +19,28 @@ export class DataFormComponent implements OnInit {
     // });
 
     this.formulario = this.formBuilder.group({
-      nome: [null],
-      email: [null]
-    })
+      nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      email: [null,[Validators.required, Validators.email]],
+    });
   }
+
+  onSubmit() {
+    console.log(this.formulario);
+
+
+    this.http
+      .post('http://httpbin.org/post', JSON.stringify(this.formulario.value))
+      .subscribe((data) => {
+        console.log(data);
+        // this.resetar()
+      },
+      // (error:any) => alert('deu ruim')
+      )
+  }
+
+  resetar(){
+    this.formulario.reset()
+  }
+
 
 }
--- src/app/data-form/data-form.module.ts
+++ src/app/data-form/data-form.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DataFormComponent } from './data-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from '../shared/shared.module';
 
 
@@ -11,7 +12,8 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule,
   ]
 })
 export class DataFormModule { }
